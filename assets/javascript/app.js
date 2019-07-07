1d99ca896654ba966daf720f0de3676e2454c2df
barfiejus <justin.kyle.barfield@gmail.com>
2019-07-07 09:55:59 -0400

    changed the objects within quizQuestions so the format can be pulled better.

---
+++ assets/javascript/app.js
@@ -0,0 +1,47 @@
+/* Sudo Code */
+
+// Maybe have a start page?
+
+// Have questions and answers loaded into question, answers, correctanswer array.
+// Set timer function for question timeout
+// Write the first question and answers on the screen.
+// Create click event to monitor the answer selected.
+    // if correct
+        // ++correct
+        // go to next question  
+    // else ++wrong or question timeout ++wrong
+// Set timeout function to show the next question automatically
+// 
+
+
+/* const myQuestions = [
+  {
+    question: "Who is the strongest?",
+    answers: {
+      a: "Superman",
+      b: "The Terminator",
+      c: "Waluigi, obviously"
+    },
+    correctAnswer: "c"
+  },
+  {
+    question: "What is the best site ever created?",
+    answers: {
+      a: "SitePoint",
+      b: "Simple Steps Code",
+      c: "Trick question; they're both the best"
+    },
+    correctAnswer: "c"
+  },
+  {
+    question: "Where is Waldo really?",
+    answers: {
+      a: "Antarctica",
+      b: "Exploring the Pacific Ocean",
+      c: "Sitting in a tree",
+      d: "Minding his own business, so stop asking"
+    },
+    correctAnswer: "d"
+  }
+];
+ */
